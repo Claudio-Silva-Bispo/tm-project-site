3471a8c8aa52c0263d3ac60976e24724ac8e64ff
Claudio Bispo <claudio_cssp@hotmail.com>
2024-05-20 01:10:30 -0300

    ajustes

--- src/app/components/DetailsFeedback.tsx
+++ src/app/components/DetailsFeedback.tsx
@@ -7,27 +7,27 @@ export default function DetailsFeedback() {
             
             <div className="container mx-auto mt-20">
                 
-                <span className="block mb-2 text-xs font-medium tracking-widest text-center uppercase dark:text-violet-600">How it works</span>
+                <span className="block mb-2 text-xs font-medium tracking-widest text-center uppercase text-terceira">How it works</span>
                 <h2 className="text-5xl font-bold text-center uppercase">Fill out our feedback form</h2>
                 
                 <div className="grid gap-6 my-16 lg:grid-cols-3">
                     
                     <div className="flex flex-col p-8 space-y-4 rounded-md dark:bg-gray-50">
-                        <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full dark:bg-violet-600 dark:text-gray-50">1</div>
+                        <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-terceira text-gray-50">1</div>
                         <p className="text-2xl font-semibold">
                         <b>Enter</b> your name and email.
                         </p>
                     </div>
 
                     <div className="flex flex-col p-8 space-y-4 rounded-md dark:bg-gray-50">
-                        <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full dark:bg-violet-600 dark:text-gray-50">2</div>
+                        <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-terceira text-gray-50">2</div>
                         <p className="text-2xl font-semibold">
                             <b>Evaluation: </b>Write your evaluation about our services.
                         </p>
                     </div>
 
                     <div className="flex flex-col p-8 space-y-4 rounded-md dark:bg-gray-50">
-                        <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full dark:bg-violet-600 dark:text-gray-50">3</div>
+                        <div className="flex items-center justify-center flex-shrink-0 w-12 h-12 text-xl font-bold rounded-full bg-terceira text-gray-50">3</div>
                         <p className="text-2xl font-semibold">
                             <b>Rating:</b> Select the number of stars for your evaluation (1 for bad, 5 for excellent).
                         </p>
