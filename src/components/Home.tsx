3093fb11592cd54bb170cdbbed72d5472d090d17
Claudio Silva Bispo <110735259+Claubis@users.noreply.github.com>
2024-07-05 11:52:16 -0300

    ajuste das imagens e espaçamentos

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ export default function HomePage(){
             
             <div className="bg-primeira">
                 
-                <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 ">
+                <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 pt-20">
                     
                     <h1 className="text-4xl font-orelega uppercase leading-none sm:text-6xl xl:max-w-3xl text-white">WELCOME TO Oliver bussiness cleaning Services</h1>
                     
--- src/pages/Teams.tsx
+++ src/pages/Teams.tsx
@@ -2,82 +2,74 @@ import React from 'react';
 import Image from 'next/image';
 
 const teamMembers = [
-    {
-      image: '/assets/Teams/imagem1.jpeg',
-      category: 'Quality',
-      date: '01',
-      title: 'Meet our team that ensures quality in every detail.',
-    },
-    {
-      image: '/assets/Teams/imagem2.jpeg',
-      category: 'Focus',
-      date: '02',
-      title: 'Absolute focus on the satisfaction and well-being of our clients.',
-    },
-    {
-      image: '/assets/Teams/imagem3.jpeg',
-      category: 'Cleanliness',
-      date: '03',
-      title: 'Excellence in cleanliness, providing an impeccable environment.',
-    },
-    {
-      image: '/assets/Teams/imagem4.jpeg',
-      category: 'Care',
-      date: '04',
-      title: 'Care and dedication in every task we perform.',
-    },
-    {
-      image: '/assets/Teams/imagem5.jpeg',
-      category: 'Organization',
-      date: '05',
-      title: 'Organization and efficiency in all our services.',
-    },
-    {
-      image: '/assets/Teams/imagem6.jpeg',
-      category: 'Professionalism',
-      date: '06',
-      title: 'Professionalism that reflects in exceptional results.',
-    },
-    {
-      image: '/assets/Teams/imagem7.jpeg',
-      category: 'Trust',
-      date: '07',
-      title: 'Trust and security in every service provided.',
-    },
-    {
-      image: '/assets/Teams/imagem8.jpeg',
-      category: 'Innovation',
-      date: '08',
-      title: 'Constant innovation for better cleaning solutions.',
-    },
-  ];
-  
+  {
+    image: '/assets/Teams/imagem1.jpeg',
+    category: 'Quality',
+    date: '01',
+    title: 'Meet our team that ensures quality in every detail.',
+  },
+  {
+    image: '/assets/Teams/imagem2.jpeg',
+    category: 'Focus',
+    date: '02',
+    title: 'Absolute focus on the satisfaction and well-being of our clients.',
+  },
+  {
+    image: '/assets/Teams/imagem3.jpeg',
+    category: 'Cleanliness',
+    date: '03',
+    title: 'Excellence in cleanliness, providing an impeccable environment.',
+  },
+  {
+    image: '/assets/Teams/imagem4.jpeg',
+    category: 'Care',
+    date: '04',
+    title: 'Care and dedication in every task we perform.',
+  },
+  {
+    image: '/assets/Teams/imagem5.jpeg',
+    category: 'Organization',
+    date: '05',
+    title: 'Organization and efficiency in all our services.',
+  },
+  {
+    image: '/assets/Teams/imagem6.jpeg',
+    category: 'Professionalism',
+    date: '06',
+    title: 'Professionalism that reflects in exceptional results.',
+  },
+  {
+    image: '/assets/Teams/imagem7.jpeg',
+    category: 'Trust',
+    date: '07',
+    title: 'Trust and security in every service provided.',
+  },
+  {
+    image: '/assets/Teams/imagem8.jpeg',
+    category: 'Innovation',
+    date: '08',
+    title: 'Constant innovation for better cleaning solutions.',
+  },
+];
 
 export default function Teams() {
   return (
-    <div className=" p-10 mx-auto dark:bg-gray-100 dark:text-gray-800 mt-20" id="Teams">
+    <div className="p-10 mx-auto dark:bg-gray-100 dark:text-gray-800 mt-20" id="Teams">
       <div className="text-center mb-10">
         <h1 className="text-4xl font-bold">Meet Our Team</h1>
         <p className="mt-4 text-lg">Our team works together to provide efficient and high-quality services.</p>
       </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 md:gap-6 lg:grid-rows-2">
         {teamMembers.map((member, index) => (
-          <div key={index} className="relative flex flex-col items-end justify-end w-full text-left bg-center bg-cover cursor-pointer min-h-[400px] group">
+          <div key={index} className="relative flex flex-col items-end justify-end w-full text-left bg-center bg-cover cursor-pointer min-h-[600px] group">
             <div className="absolute inset-0 bg-gradient-to-b from-transparent to-gray-800 opacity-70"></div>
-            {member.image.startsWith('http') ? (
-              <div
-                className="absolute inset-0 bg-center bg-cover"
-                style={{ backgroundImage: `url(${member.image})` }}
-              ></div>
-            ) : (
-              <Image
-                src={member.image}
-                alt={member.category}
-                layout="fill"
-                objectFit="fill"
-                className="absolute inset-0"
-              />
-            )}
+            <Image
+              src={member.image}
+              alt={member.category}
+              layout="fill"
+              objectFit="cover"
+              className="absolute inset-0"
+            />
             <div className="absolute top-0 left-0 right-0 flex items-center justify-between p-5">
               <a
                 rel="noopener noreferrer"
