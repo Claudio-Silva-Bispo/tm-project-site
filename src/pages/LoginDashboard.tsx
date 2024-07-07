23d0c434529efd35b6eff49b0e935d0d9cf66d40
Claudio Silva Bispo <110735259+Claubis@users.noreply.github.com>
2024-07-07 08:50:23 -0300

    troca do link da api

--- src/pages/LoginDashboard.tsx
+++ src/pages/LoginDashboard.tsx
@@ -14,7 +14,7 @@ export default function LoginDashboard() {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:3000/api/login', {
+      const response = await fetch('https://olivercleaningservice.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
